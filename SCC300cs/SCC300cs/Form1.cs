aca64f7ee33a24ea46440ff8bb00ff3e02fb6b01
Luca Davies <luca-davies@outlook.com>
2019-11-05 14:04:33 +0000

    Added CoreNLP package

--- SCC300cs/SCC300cs/Form1.cs
+++ SCC300cs/SCC300cs/Form1.cs
@@ -16,5 +16,10 @@ namespace SCC300cs
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- SCC300cs/SCC300cs/Program.cs
+++ SCC300cs/SCC300cs/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using edu.stanford.nlp.simple;
 
 namespace SCC300cs
 {
@@ -17,6 +18,7 @@ namespace SCC300cs
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
