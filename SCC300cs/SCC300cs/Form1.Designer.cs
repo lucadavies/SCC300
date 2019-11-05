ee5f9f79ccb4c405ed53cb4183bbd81b58fa035e
Luca Davies <luca-davies@outlook.com>
2019-11-05 14:25:28 +0000

    Text read in from file

--- SCC300cs/SCC300cs/Form1.Designer.cs
+++ SCC300cs/SCC300cs/Form1.Designer.cs
@@ -28,21 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Location = new System.Drawing.Point(12, 415);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(75, 23);
+            this.BtnLoad.TabIndex = 0;
+            this.BtnLoad.Text = "Load";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(776, 397);
+            this.txtInput.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.BtnLoad);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.TextBox txtInput;
     }
 }
 
--- SCC300cs/SCC300cs/Form1.cs
+++ SCC300cs/SCC300cs/Form1.cs
@@ -12,6 +12,8 @@ namespace SCC300cs
 {
     public partial class Form1 : Form
     {
+        string inputText;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +22,23 @@ namespace SCC300cs
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                ofd.FilterIndex = 2;
+                ofd.RestoreDirectory = true;
+                
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    inputText = System.IO.File.ReadAllText(ofd.FileName);
+                }
+            }
+            txtInput.Text = inputText;
         }
     }
 }
