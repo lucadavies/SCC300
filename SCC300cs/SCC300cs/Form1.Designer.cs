616115b6f9e47bb660ad8558ccfbb3c0da125973
Luca Davies <luca-davies@outlook.com>
2019-12-27 17:16:12 +0000

    Fixed. About 7-8 commit hisotry lost in the screw up, data still here.
    Got pipeline working, promptly abandoned Stanford Setniment analysis in favour of VADER, added in chart to display VADER output (VADER in use going forward)

--- SCC300cs/SCC300cs/Form1.Designer.cs
+++ SCC300cs/SCC300cs/Form1.Designer.cs
@@ -28,16 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.BtnLoad = new System.Windows.Forms.Button();
             this.txtInput = new System.Windows.Forms.TextBox();
-            this.btnTokenize = new System.Windows.Forms.Button();
-            this.btnLem = new System.Windows.Forms.Button();
             this.btnAnnotate = new System.Windows.Forms.Button();
+            this.bgWkrAnnotate = new System.ComponentModel.BackgroundWorker();
+            this.picLoading = new System.Windows.Forms.PictureBox();
+            this.panLoading = new System.Windows.Forms.Panel();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnVaderSenti = new System.Windows.Forms.Button();
+            this.bgWkrSenti = new System.ComponentModel.BackgroundWorker();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.picLoading)).BeginInit();
+            this.panLoading.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnLoad
             // 
-            this.BtnLoad.Location = new System.Drawing.Point(12, 415);
+            this.BtnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BtnLoad.Location = new System.Drawing.Point(12, 426);
             this.BtnLoad.Name = "BtnLoad";
             this.BtnLoad.Size = new System.Drawing.Size(75, 23);
             this.BtnLoad.TabIndex = 0;
@@ -47,36 +69,20 @@
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(6, 6);
             this.txtInput.Multiline = true;
             this.txtInput.Name = "txtInput";
             this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtInput.Size = new System.Drawing.Size(776, 397);
+            this.txtInput.Size = new System.Drawing.Size(761, 376);
             this.txtInput.TabIndex = 1;
-            // 
-            // btnTokenize
-            // 
-            this.btnTokenize.Location = new System.Drawing.Point(93, 415);
-            this.btnTokenize.Name = "btnTokenize";
-            this.btnTokenize.Size = new System.Drawing.Size(75, 23);
-            this.btnTokenize.TabIndex = 2;
-            this.btnTokenize.Text = "Tokenize";
-            this.btnTokenize.UseVisualStyleBackColor = true;
-            this.btnTokenize.Click += new System.EventHandler(this.BtnTokenize_Click);
-            // 
-            // btnLem
-            // 
-            this.btnLem.Location = new System.Drawing.Point(174, 415);
-            this.btnLem.Name = "btnLem";
-            this.btnLem.Size = new System.Drawing.Size(75, 23);
-            this.btnLem.TabIndex = 3;
-            this.btnLem.Text = "Lemmatize";
-            this.btnLem.UseVisualStyleBackColor = true;
-            this.btnLem.Click += new System.EventHandler(this.btnLem_Click);
             // 
             // btnAnnotate
             // 
-            this.btnAnnotate.Location = new System.Drawing.Point(255, 415);
+            this.btnAnnotate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAnnotate.Location = new System.Drawing.Point(93, 426);
             this.btnAnnotate.Name = "btnAnnotate";
             this.btnAnnotate.Size = new System.Drawing.Size(75, 23);
             this.btnAnnotate.TabIndex = 4;
@@ -84,21 +90,169 @@
             this.btnAnnotate.UseVisualStyleBackColor = true;
             this.btnAnnotate.Click += new System.EventHandler(this.BtnAnnotate_Click);
             // 
+            // bgWkrAnnotate
+            // 
+            this.bgWkrAnnotate.WorkerReportsProgress = true;
+            this.bgWkrAnnotate.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWkrAnnotate_DoWork_AnnotateText);
+            this.bgWkrAnnotate.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWkrAnnotate_RunWorkerCompleted);
+            // 
+            // picLoading
+            // 
+            this.picLoading.Image = ((System.Drawing.Image)(resources.GetObject("picLoading.Image")));
+            this.picLoading.Location = new System.Drawing.Point(3, 3);
+            this.picLoading.Name = "picLoading";
+            this.picLoading.Size = new System.Drawing.Size(200, 200);
+            this.picLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLoading.TabIndex = 6;
+            this.picLoading.TabStop = false;
+            // 
+            // panLoading
+            // 
+            this.panLoading.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panLoading.Controls.Add(this.lblLoading);
+            this.panLoading.Controls.Add(this.picLoading);
+            this.panLoading.Location = new System.Drawing.Point(300, 120);
+            this.panLoading.Name = "panLoading";
+            this.panLoading.Size = new System.Drawing.Size(206, 236);
+            this.panLoading.TabIndex = 7;
+            this.panLoading.Visible = false;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Location = new System.Drawing.Point(74, 206);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(54, 13);
+            this.lblLoading.TabIndex = 7;
+            this.lblLoading.Text = "Loading...";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(2, 9);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(781, 414);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtInput);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(773, 388);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Raw";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtOutput);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(773, 388);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Processed";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(6, 6);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(761, 376);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // btnVaderSenti
+            // 
+            this.btnVaderSenti.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnVaderSenti.Location = new System.Drawing.Point(174, 426);
+            this.btnVaderSenti.Name = "btnVaderSenti";
+            this.btnVaderSenti.Size = new System.Drawing.Size(75, 23);
+            this.btnVaderSenti.TabIndex = 9;
+            this.btnVaderSenti.Text = "Vader Senti.";
+            this.btnVaderSenti.UseVisualStyleBackColor = true;
+            this.btnVaderSenti.Click += new System.EventHandler(this.btnVaderSenti_Click);
+            // 
+            // bgWkrSenti
+            // 
+            this.bgWkrSenti.WorkerReportsProgress = true;
+            this.bgWkrSenti.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWkrSenti_DoWork_AnalyseText);
+            this.bgWkrSenti.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWkrSenti_RunWorkerCompleted);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.chart);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(773, 388);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Results";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.Title = "Beginning / Electricity";
+            chartArea1.AxisY.Title = "Good / Bad";
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(6, 8);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Default";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(760, 377);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnVaderSenti);
+            this.Controls.Add(this.panLoading);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.btnAnnotate);
-            this.Controls.Add(this.btnLem);
-            this.Controls.Add(this.btnTokenize);
-            this.Controls.Add(this.txtInput);
             this.Controls.Add(this.BtnLoad);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 500);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picLoading)).EndInit();
+            this.panLoading.ResumeLayout(false);
+            this.panLoading.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -106,9 +260,19 @@
 
         private System.Windows.Forms.Button BtnLoad;
         private System.Windows.Forms.TextBox txtInput;
-        private System.Windows.Forms.Button btnTokenize;
-        private System.Windows.Forms.Button btnLem;
         private System.Windows.Forms.Button btnAnnotate;
+        private System.ComponentModel.BackgroundWorker bgWkrAnnotate;
+        private System.Windows.Forms.PictureBox picLoading;
+        private System.Windows.Forms.Panel panLoading;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnVaderSenti;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.ComponentModel.BackgroundWorker bgWkrSenti;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
     }
 }
 
--- SCC300cs/SCC300cs/Form1.cs
+++ SCC300cs/SCC300cs/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,29 +11,49 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using org.ejml.simple;
 using edu.stanford.nlp.ling;
-using edu.stanford.nlp.neural.rnn;
 using edu.stanford.nlp.pipeline;
-using edu.stanford.nlp.sentiment;
-using edu.stanford.nlp.trees;
-using edu.stanford.nlp.util;
+using edu.stanford.nlp.simple;
+using VaderSharp;
 
 namespace SCC300cs
 {
     public partial class Form1 : Form
     {
         string inputText;
-        Document doc;
         StanfordCoreNLP pipeline;
+        List<string> sents;
+        List<SentimentAnalysisResults> resultsList;
 
         public Form1()
         {
             InitializeComponent();
-            pipeline = new StanfordCoreNLP(@"C:\Users\Luca\Documents\University\General\UG3\SCC300\SCC300cs\SCC300cs\stanford-corenlp-3.7.0-models\edu\stanford\nlp\models");
+            initPipeline();
+        }
+
+        private void initPipeline()
+        {
+            var jarRoot = @"C:\Users\Luca\Documents\University\General\UG3\SCC300\SCC300cs\SCC300cs\stanford-corenlp-3.7.0-models";
+
+            // Annotation pipeline configuration
+            var props = new java.util.Properties();
+            props.setProperty("annotators", "tokenize, ssplit");
+            props.setProperty("ner.useSUTime", "0");
+
+            var CurDir = Environment.CurrentDirectory;
+            Directory.SetCurrentDirectory(jarRoot);         //change working dir to locate models
+            pipeline = new StanfordCoreNLP(props);
+            Directory.SetCurrentDirectory(CurDir);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Random rnd = new Random();
+            chart.Series["Default"].Points.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                chart.Series["Default"].Points.AddXY(i + 1, rnd.Next(i * 10, i * 20));
+            }
+            
         }
 
         private void BtnLoad_Click(object sender, EventArgs e)
@@ -43,10 +64,10 @@ namespace SCC300cs
                 ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
                 ofd.FilterIndex = 2;
                 ofd.RestoreDirectory = true;
-                
+
                 if (ofd.ShowDialog() == DialogResult.OK)
                 {
-                    inputText = System.IO.File.ReadAllText(ofd.FileName);
+                    inputText = File.ReadAllText(ofd.FileName);
                 }
             }
             Regex rgx = new Regex("[^\r\n]{2}\r\n[^\r\n]{2}");
@@ -60,39 +81,69 @@ namespace SCC300cs
 
         private void BtnAnnotate_Click(object sender, EventArgs e)
         {
-            Annotation doc = new Annotation(inputText);
-            pipeline.annotate(doc);
-
+            if (txtInput.Text != "")
+            {
+                panLoading.Visible = true;
+                bgWkrAnnotate.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Please load text to be processed.", "Error");
+            }
         }
 
-        private void BtnTokenize_Click(object sender, EventArgs e)
+        private void btnVaderSenti_Click(object sender, EventArgs e)
         {
-            //doc = new Document(inputText);
-            //java.util.List sens = doc.sentences();
-            //txtInput.Clear();
-            //for (int i = 0; i < sens.size(); i++)
-            //{
-            //    txtInput.AppendText(sens.get(i).ToString());
-            //    txtInput.AppendText(Environment.NewLine);
-            //}
+            panLoading.Visible = true;
+            bgWkrSenti.RunWorkerAsync();
         }
 
-        private void btnLem_Click(object sender, EventArgs e)
+        private void bgWkrAnnotate_DoWork_AnnotateText(object sender, DoWorkEventArgs e)
         {
-            //java.util.Properties props = new java.util.Properties();
-            //props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
-            //java.util.List sens = doc.sentences();
-            //txtInput.Clear();
-            //for (int i = 0; i < sens.size(); i++)
-            //{
-            //    java.util.List lems = ((Sentence)sens.get(i)).lemmas();
-            //    for (int j = 0; j < lems.size(); j++)
-            //    {
-            //        txtInput.AppendText(lems.get(j).ToString());
-            //        txtInput.AppendText(" ");
-                    
-            //    }
-            //}
+            edu.stanford.nlp.simple.Document doc = new edu.stanford.nlp.simple.Document(inputText);
+            e.Result = doc.sentences();
+        }
+
+        private void bgWkrAnnotate_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            java.util.List jSents = (java.util.List)e.Result;
+            txtInput.Text = "";
+            inputText = "";
+            sents = new List<string>();
+            for (java.util.Iterator itr = jSents.iterator(); itr.hasNext();)
+            {
+                sents.Add(((Sentence)itr.next()).text());
+                inputText = inputText + sents[sents.Count - 1] + Environment.NewLine;
+            }
+            txtInput.Text = inputText;
+            panLoading.Visible = false;
+        }
+
+        private void bgWkrSenti_DoWork_AnalyseText(object Sender, DoWorkEventArgs e)
+        {
+            SentimentIntensityAnalyzer analyzer = new SentimentIntensityAnalyzer();
+            SentimentAnalysisResults results;
+            resultsList = new List<SentimentAnalysisResults>();
+            string op = "";
+            txtOutput.Text = "";
+            foreach (string s in sents)
+            {
+                results = analyzer.PolarityScores(s);
+                resultsList.Add(results);
+                op = op + "Sentence [" + (sents.IndexOf(s) + 1) + "]: " + s + Environment.NewLine;
+                op = op + "Positive: " + results.Positive + " | ";
+                op = op + "Negative: " + results.Negative + " | ";
+                op = op + "Neutral: " + results.Neutral + " | ";
+                op = op + "Compound: " + results.Compound;
+                op = op + Environment.NewLine + Environment.NewLine;
+            }
+            e.Result = op;
+        }
+
+        private void bgWkrSenti_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            txtOutput.Text = (string)e.Result;
+            panLoading.Visible = false;
         }
 
         private string Replace(string s, int index, int length, string replacement)
